4ed6f49709abe496bde573745f8cac2f4a0919c5
Alejo <Alejo@Alejo-PC>
2016-03-22 18:50:38 -0500

    Iniciar repositorio Bases de datos 2016

---
+++ src/basedatos/MainPruebaBD.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package basedatos;
+
+/**
+ *
+ * @author Alejo
+ */
+public class MainPruebaBD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        BaseDatos objBases= new BaseDatos();
+        boolean conexion;
+        String sql="";
+        conexion=objBases.crearConexion();
+        if(conexion){
+            sql="insert into profesores (identificacion, nombre, apellido, telefono) values('11345','Carlos','Pelaez', '32133')";
+            objBases.ejecutarSQL(sql);
+            System.out.println("conexión exitosa");
+        }else{
+            System.out.println("no se pudo realizar la conexión");
+        }
+    }
+    
+}
