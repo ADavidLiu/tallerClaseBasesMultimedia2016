1bc42954840627a0e4d38ce4d3e1ca50ab354058
Alejo <Alejo@Alejo-PC>
2016-03-22 19:24:00 -0500

    En la clase profesor se crean los atributos con sus get y set, además de los constructores

--- src/basedatos/Profesores.java
+++ src/basedatos/Profesores.java
@@ -10,5 +10,72 @@ package basedatos;
  * @author Alejo
  */
 public class Profesores {
-    
+
+    private String nombre;
+    private String apellido;
+    private String identificacion;
+
+    public Profesores() {
+    }
+
+    public Profesores(String nombre, String apellido, String identificacion) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.identificacion = identificacion;
+    }
+
+    /**
+     * Get the value of identificacion
+     *
+     * @return the value of identificacion
+     */
+    public String getIdentificacion() {
+        return identificacion;
+    }
+
+    /**
+     * Set the value of identificacion
+     *
+     * @param identificacion new value of identificacion
+     */
+    public void setIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    /**
+     * Get the value of apellido
+     *
+     * @return the value of apellido
+     */
+    public String getApellido() {
+        return apellido;
+    }
+
+    /**
+     * Set the value of apellido
+     *
+     * @param apellido new value of apellido
+     */
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    /**
+     * Get the value of nombre
+     *
+     * @return the value of nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * Set the value of nombre
+     *
+     * @param nombre new value of nombre
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
 }
